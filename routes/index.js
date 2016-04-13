f283b1ef689ca4592afdf8a244339b0e35be7321
Brian <brian.steven.davis@gmail.com>
2016-04-13 00:03:42 +0000

    first commit

---
+++ routes/index.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var fs = require('fs');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.sendFile('weather.html', { root:  'public' });
+});
+router.get('/getcity', function(req, res) {
+	console.log("In getcity");
+	fs.readFile(__dirname + '/cities.dat.txt',function(err,data) 
+	{
+		if(err) throw err;
+		var cities = data.toString().split("\n");
+		for(var i = 0; i < cities.length; i++)
+		{
+			var myRe = new RegExp("^" + req.query.q);
+			var result = cities[i].search(myRe);
+					}
+	
+		var jsonresult = [];
+        for(var i = 0; i < cities.length; i++) 
+		{
+			var result = cities[i].search(myRe); 
+			if(result != -1) 
+			{
+				console.log(cities[i]);
+				jsonresult.push({city:cities[i]});
+			} 
+        }   
+        console.log(jsonresult);
+		res.status(200).json(jsonresult);
+	});
+});
+
+module.exports = router;
